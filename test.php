8c50d67f97c596d4d873b42727f0cf2440b8838b
Ahsan Habib <ahnazir.ahn@gmail.com>
2023-02-06 22:10:12 +0600

    condition updated for invalid data

--- test.php
+++ test.php
@@ -1,9 +1,8 @@
 <?php
 
-$tuition_fee = 18000;
+$tuition_fee = 1700;
 $commission = ($tuition_fee > 20000) ? 0.25 * $tuition_fee : 
               (($tuition_fee > 10000) ? 0.20 * $tuition_fee : 
-              (($tuition_fee > 7000) ? 0.15 * $tuition_fee : 
-              (($tuition_fee >= 0) ? "Invalid Data" : 
-              "Invalid Data")));
+              (($tuition_fee > 7000) ? 0.15 * $tuition_fee :"Invalid"
+              ));
 echo "For a tuition fee of $tuition_fee, the commission is $commission";
