7f466592ddd7033a724bf53be149228b8b7b6795
Ahsan Habib <ahnazir.ahn@gmail.com>
2023-02-06 22:15:19 +0600

    $ symbool Added while printing Output

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <?php
 
-$tuition_fee = 1700;
+$tuition_fee = 250000;
 $commission = ($tuition_fee > 20000) ? 0.25 * $tuition_fee : 
               (($tuition_fee > 10000) ? 0.20 * $tuition_fee : 
               (($tuition_fee > 7000) ? 0.15 * $tuition_fee :"Invalid"
               ));
-echo "For a tuition fee of $tuition_fee, the commission is $commission";
+echo "For a tuition fee of \$$tuition_fee, the commission is \$$commission";
